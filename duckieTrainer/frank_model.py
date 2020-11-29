c94f72d4d0ee0185fe6373c93466a192d913f34a
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-11-29 16:16:15 -0500

    combined prediction to 1

--- duckieTrainer/frank_model.py
+++ duckieTrainer/frank_model.py
@@ -3,8 +3,9 @@ from tensorflow.keras.layers import Conv2D,Activation,Lambda,Flatten,Dense
 #! Model in reference to Nvidia's Behavior Cloning Paper.
 
 class FrankNet:
+
     @staticmethod
-    def build_linear_branch(inputs=(150, 200, 3)):
+    def build_branch(inputs=(150, 200, 3)):
         # ? Input Normalization
         x = Lambda(lambda x: x/255.0)(inputs)
 
@@ -32,40 +33,7 @@ class FrankNet:
         x = Dense(100, kernel_initializer='normal', activation='relu')(x)
         x = Dense(50, kernel_initializer='normal', activation='relu')(x)
         x = Dense(10, kernel_initializer='normal', activation='relu')(x)
-        x = Dense(1, kernel_initializer='normal', name="Linear")(x)
-
-        return x
-
-    @staticmethod
-    def build_angular_branch(inputs=(150, 200, 3)):
-        # ? Input Normalization
-        x = Lambda(lambda x: x/255.0)(inputs)
-
-        # ? L1: CONV => RELU
-        x = Conv2D(24, (5, 5), strides=(2, 2), padding="valid")(x)
-        x = Activation("relu")(x)
-        # ? L2: CONV => RELU
-        x = Conv2D(36, (5, 5), strides=(2, 2), padding="valid")(x)
-        x = Activation("relu")(x)
-        # ? L3: CONV => RELU
-        x = Conv2D(48, (5, 5), strides=(2, 2), padding="valid")(x)
-        x = Activation("relu")(x)
-        # ? L4: CONV => RELU
-        x = Conv2D(64, (3, 3), padding="valid")(x)
-        x = Activation("relu")(x)
-        # ? L5: CONV => RELU
-        x = Conv2D(64, (3, 3), padding="valid")(x)
-        x = Activation("relu")(x)
-
-        # ? Flatten
-        x = Flatten()(x)
-
-        # ? Fully Connected
-        x = Dense(1164, kernel_initializer='normal', activation='relu')(x)
-        x = Dense(100, kernel_initializer='normal', activation='relu')(x)
-        x = Dense(50, kernel_initializer='normal', activation='relu')(x)
-        x = Dense(10, kernel_initializer='normal', activation='relu')(x)
-        x = Dense(1, kernel_initializer='normal', name="Angular")(x)
+        x = Dense(2, kernel_initializer='normal', name="Prediction")(x)
 
         return x
 
@@ -73,10 +41,8 @@ class FrankNet:
     def build(width=150, height=200):
         input_shape = (height, width, 3)
         inputs = tf.keras.Input(shape=input_shape)
-        linearVelocity = FrankNet.build_linear_branch(inputs)
-        angularVelocity = FrankNet.build_angular_branch(inputs)
+        prediction = FrankNet.build_branch(inputs)
 
-        model = tf.keras.Model(inputs=inputs, outputs=[
-            linearVelocity, angularVelocity], name="FrankNet")
+        model = tf.keras.Model(inputs=inputs, outputs=prediction, name="FrankNet")
 
         return model
