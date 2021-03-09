0489f4a19a56ec95bee2db2ed7fca75af3a28d74
frank-qcd-qk <frank_chude.qian@outlook.com>
2021-03-09 04:06:13 -0500

    added visualizer

--- duckieModels/testModelv2.py
+++ duckieModels/testModelv2.py
@@ -63,6 +63,46 @@ def model_vis(model):
     plt.show()
 
 
+def log_visualize_attention(model, logdir="Mar8_zig_zag_more_ducks.log"):
+    from duckieLog.log_util import read_dataset
+    observation, prediction, anomaly = read_dataset(
+        logdir
+    )
+    frames = []
+    frame_count = 0
+    bad = 0
+    total = len(observation)
+    for a_observation, a_gt in zip(observation, anomaly):
+        frame_count += 1
+        observation = np.expand_dims(a_observation, axis=0)
+        output = model.predict(observation)
+        heatmap = visualize_cam(model, layer_idx=-1, filter_indices=0,
+                                seed_input=a_observation, grad_modifier=None)
+        jet_heatmap = cv2.cvtColor(np.uint8(cm.jet(heatmap)[..., :3] * 255), cv2.COLOR_BGR2RGB)
+        plt_img = cv2.cvtColor(a_observation, cv2.COLOR_YUV2RGB)
+        fin = cv2.addWeighted(plt_img, 0.6, jet_heatmap, 0.4, 0)
+        result = (output[0][0] > 0.5) == bool(a_gt)
+        if not result:
+            bad += 1
+        print(
+            "Frame {}, Guess: {}, GT: {}, result: {}".format(frame_count, round(float(output[0][0]), 2), a_gt, result))
+        string_to_show = "Guess: {}, GT: {}".format(round(float(output[0][0]), 2), a_gt)
+        x, y, w, h = 0, 0, 30, 30
+        # Add text
+        cv2.putText(fin, string_to_show, (x + int(w / 10), y + int(h / 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (255, 255, 255), 2)
+        frames.append(fin)
+        cv2.imshow("Output", fin)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    print("Correctness: {}%".format((total - bad) / total))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (200, 150))
+    for i in range(len(frames)):
+        out.write(frames[i])
+    out.release()
+
+
 if __name__ == "__main__":
     loaded_model = cbcNetv2.get_anomaly_inference(weigths="cbcNet-anomaly-Best_Validation.h5")
-    model_vis(loaded_model)
+    log_visualize_attention(loaded_model)
